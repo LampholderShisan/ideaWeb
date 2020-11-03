0bdfd1ef2eb331811e9befdf17efc753ccdaac5b
NanQiao <657997691qq.com>
2020-11-03 22:43:31 +0800

    GradleWeb...init

---
+++ src/main/java/org/GradleWeb/Servlet.java
@@ -0,0 +1,30 @@
+package org.GradleWeb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/MyServlet")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("do post");
+        int a = 10;
+        int b =30;
+        int c =a+b;
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("do get");
+        int a = 10;
+ZZ        int b =30;
+        int c =a+b;
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
