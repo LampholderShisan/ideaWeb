3610cb2a09018722ab4a1d91e0a757af2a3519bc
NanQiao <657997691qq.com>
2020-11-04 16:22:39 +0800

    修改。。。。。

--- src/main/java/org/GradleWeb/Servlet.java
+++ src/main/java/org/GradleWeb/Servlet.java
@@ -1,31 +1,24 @@
 package org.GradleWeb;
-//怎么搞
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+////注释。。。。。。。
 import java.io.IOException;
 
-@WebServlet("/MyServlet")
-public class Servlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+@WebServlet("/Servlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("do post");
-        int a = 10;
-        int b =30;
-        int c =a+b;
+        int a =20 ;
+        int b = 50;
+        int c = a+b;
+        System.out.println(a);
         System.out.println(c);
-        System.out.println(b);
-
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("do get");
-        int a = 10;
-        int b =30;
-        int c =a+b;
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("do post");
+        int a =20 ;
+        int b = 50;
+        int c = a+b;
+        System.out.println(a);
         System.out.println(c);
-        System.out.println(b);
     }
 }
